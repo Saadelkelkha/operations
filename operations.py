dd760ad97ea0d81c66af659bccfc189f5407133d
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-09 15:21:01 +0100

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,15 @@
+A=int(input("type the first number :"))
+B=int(input("type the second number :"))
+OP=input("type the operation :")
+if OP == "+" :
+  print("the result is :", A+B)
+elif OP == "-" :
+  print("the result is :", A-B)
+elif OP == "*" :
+ print("the result is :", A*B)
+elif OP == "/" :
+ print("the result is :",A/B)
+elif OP == "%" :
+ print("the result is :",A%B)
+else :
+ print("operator does not exist")
